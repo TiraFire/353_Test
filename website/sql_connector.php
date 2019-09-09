8936264e14b77cf89fd262966efac44aec1b2729
WilliamCui <33232366+TaillessSheep@users.noreply.github.com>
2019-09-09 18:57:33 -0400

    Auto config DB in PHP

--- website/sql_connector.php
+++ website/sql_connector.php
@@ -1,6 +1,9 @@
 
 <?php
 
+// Name of the file
+$filename = 'database_config.sql';
+
 $user = 'root';
 
 $password = '';
@@ -8,5 +11,33 @@ $password = '';
 $database = 'website_db';
 
 $db = new mysqli('localhost', $user, $password, $database) or die("Connection failed");
+
+
+// Temporary variable, used to store current query
+$templine = '';
+// Read in entire file
+$lines = file($filename);
+// Loop through each line
+foreach ($lines as $line)
+{
+// Skip it if it's a comment
+if (substr($line, 0, 2) == '--' || $line == '')
+    continue;
+
+// Add this line to the current segment
+$templine .= $line;
+// If it has a semicolon at the end, it's the end of the query
+if (substr(trim($line), -1, 1) == ';')
+{
+    // Perform the query
+    //$db->query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+    $db->query($templine) or print("DB config failed.");
+    //new mysqli($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+    // Reset temp variable to empty
+    $templine = '';
+}
+}
+ echo "Tables imported successfully";
+
 
 ?>
